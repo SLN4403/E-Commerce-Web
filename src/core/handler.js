0f70288dc3a53607597337ad9cbcbb89ee47f9de
Su Lei <suleinandar3044@gmail.com>
2023-11-20 00:47:11 +0630

    side bar open/close pt-3 done

--- src/core/handler.js
+++ src/core/handler.js
@@ -3,6 +3,7 @@ import { cartBar } from "./selector";
 export const cartBtnHandler = () => {
     // console.log("cartBar");
     cartBar.classList.toggle("translate-x-full")
+    !cartBar.classList.toggle("duration-300") && cartBar.classList.toggle("duration-300");
 }
 
 export const closeCartBtnHandler = () => {
